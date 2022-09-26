3cb773c9c3361f38c874da3c35861e17a930a206
fadilaahmads <fadilahmad59@gmail.com>
2022-09-26 21:49:23 +0700

    Add GetById func

--- controller/ctrl_shop_impl.go
+++ controller/ctrl_shop_impl.go
@@ -1,7 +1,11 @@
 package controller
 
 import (
+	"net/http"
+	"personal-finance/models/web"
 	"personal-finance/services"
+	"personal-finance/utils"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,7 +25,17 @@ func (controller *ShopControllerImpl) Create(c *gin.Context) {
 }
 
 func (controller *ShopControllerImpl) GetById(c *gin.Context) {
+	id := c.Param("id")
+	idInt, err := strconv.Atoi(id)
+	utils.PanicIfError(err)
 
+	ShopResponse := controller.ShopService.GetById(c, idInt)
+	WebResponse := web.WebResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   ShopResponse,
+	}
+	c.JSON(http.StatusOK, WebResponse)
 }
 
 func (controller *ShopControllerImpl) GetAll(c *gin.Context) {
